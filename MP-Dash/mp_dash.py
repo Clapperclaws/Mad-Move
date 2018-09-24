aa1b727e58b321c1cdc8067517f93e8b5884e3fe
sayoubi <sara.ayoubi@hotmail.com>
2018-09-24 09:59:31 +0200

    Updated the MP-DASH script to include some minor fixes

--- MP-Dash/mp_dash.py
+++ MP-Dash/mp_dash.py
@@ -4,10 +4,12 @@ class mp_dash:
 
     def __init__(self, start_time, chunk_size, alfa):
 
-        self.chunk_size = chunk_size
-        self.star_time = start_time
-        self.remaining_bytes = chunk_size
-        self.alfa = alfa
+        self.chunk_size = chunk_size # Synonymous to S - video chunk size
+        self.sent_bytes = 0 # Total amount of bytes sent so far
+        self.start_time = start_time # Time when HTTP Get request is sent
+
+        self.alfa = alfa # Alfa is used to compensate the estimation inaccuracy of Wifi Throughput
+        self.packet_size = 1500
 
         # Get Buffer Level from JS
         #start a connection to the server
@@ -17,25 +19,36 @@ class mp_dash:
         #Read Reply from Server
         result = ws.recv()
         #This stripping & trimming is to handle the javascript output function -- could be modified
-        self.buffer_level = float(str.strip(result.split(':')[1]))
-        print self.buffer_level
+        self.deadline_window = float(str.strip(result.split(':')[1]))
         ws.close()
 
     #MP-DASH function to enable/disable cellular sub-flow -- assumes as input, timenow, wifi-throughput,
     # amount of sent-bytes, boolean if Cellular subflow is enabled/disabled
-    def on_packet_received(self, time_now, r_wifi, sent_bytes, is_cell_on, lte_throughput, lte_off):
+    # This function return 0 to disable the cellular path and 1 to enable the cellular path
+    def on_packet_received(self, time_now, r_wifi, lte_throughput, lte_off):
+        n = 0
+        #At every call of the on-packet received function -- pretend that a MSS is received.
+        if self.chunk_size - self.sent_bytes > self.packet_size:
+            n = self.packet_size
+        else:
+            n = self.chunk_size - self.sent_bytes
 
-        time_spent = time_now - self.star_time
-        self.remaining_bytes = self.remaining_bytes - sent_bytes
+        # Update the total amount of bytes sent
+        self.sent_bytes += n
 
-        if(self.remaining_bytes == 0):
+        # Get time that elapses between HTTP Get and HTTP Response
+        time_spent = time_now - self.start_time
+
+        #If amount of remaining bytes is 0 -- last packet for this chunk
+        if(self.chunk_size - self.sent_bytes <= 0):
             ws = create_connection("ws://localhost:9001")
+            #Send lte-info to the javascript -- lte_throughput and time lte is off (comma separated).
             ws.send("lte_response:"+str(lte_throughput)+","+str(lte_off))
 
-        if ((self.alfa * self.buffer_level - time_spent) * r_wifi) > (self.chunk_size - sent_bytes):
-            if is_cell_on:
-                return 0
+        if ((self.alfa * self.deadline_window - time_spent) * r_wifi) > (self.chunk_size - self.sent_bytes):
+            # if is_cell_on:
+                return 0 # Disable the cellular path
 
-        if ((self.alfa * self.buffer_level - time_spent) * r_wifi) < (self.chunk_size - sent_bytes):
-            if not is_cell_on:
-                return 1
+        if ((self.alfa * self.deadline_window - time_spent) * r_wifi) <= (self.chunk_size - self.sent_bytes):
+            # if not is_cell_on:
+                return 1 # Enable the cellular path
